8776fd9acc49cfffefccb0c4d91d5dcf3de906af
Donovan Price <donovan.price@pricelab.net.au>
2024-05-03 16:00:08 +1000

    chore: added provisioner.sh

--- shellScript/cnflab-provisioner.sh
+++ shellScript/cnflab-provisioner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-APT_PACKAGES="net-tools"
+APT_PACKAGES="net-tools bash-completion"
 for package in ${APT_PACKAGES} ; do
     sudo apt install ${package}
 done
@@ -13,3 +13,22 @@ done
 mkdir -p .kube
 cp ~/build/k3sKubeConfig/10.1.1.6/etc/rancher/k3s/k3s.yaml ~/.kube/config
 sed -i 's/https:\/\/127.0.0.1:6443/https:\/\/10.1.1.6:6443/g' ~/.kube/config
+
+# Setup Kubectl AutoCompletion
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+
+# Kubectl shortcuts
+echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'alias kgp=kubectl get pods' >> ~/.bashrc
+echo 'alias kgs=kubectl get svc' >> ~/.bashrc
+echo 'alias kdp=kubectl describe pod' >> ~/.bashrc
+echo 'alias kl=kubectl logs -f' >> ~/.bashrc
+echo 'alias klf=kubectl logs -f' >> ~/.bashrc
+echo 'alias kxb=kubectl exec -it' >> ~/.bashrc
+echo 'alias kdesc=kubectl describe' >> ~/.bashrc
+echo 'alias kgettmm=kubectl get pods -owide  | grep tmm | sort -k8' >> ~/.bashrc
+echo 'alias kgrs=kubectl get rs ' >> ~/.bashrc
+echo 'alias kdrs=kubectl describe rs ' >> ~/.bashrc
+echo 'alias kgd=kubectl get deploy ' >> ~/.bashrc

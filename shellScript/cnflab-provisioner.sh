2e0c4b4034eff167fe17184fcf7159f00db3270f
Donovan Price <donovan.price@pricelab.net.au>
2024-05-03 16:12:55 +1000

    chore: added provisioner.sh

--- shellScript/cnflab-provisioner.sh
+++ shellScript/cnflab-provisioner.sh
@@ -20,17 +20,17 @@ kubectl completion bash > ~/.bash_kube_completion
 # Kubectl shortcuts
 echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
 
-echo 'alias k=\'kubectl\'' >> ~/.bashrc
-echo 'alias kgp=\'kubectl get pods\'' >> ~/.bashrc
-echo 'alias kgs=\'kubectl get svc\'' >> ~/.bashrc
-echo 'alias kdp=\'kubectl describe pod\'' >> ~/.bashrc
-echo 'alias kl=\'kubectl logs -f\'' >> ~/.bashrc
-echo 'alias klf=\'kubectl logs -f\'' >> ~/.bashrc
-echo 'alias kxb=\'kubectl exec -it\'' >> ~/.bashrc
-echo 'alias kdesc=\'kubectl describe\'' >> ~/.bashrc
-echo 'alias kgettmm=\'kubectl get pods -owide  | grep tmm | sort -k8\'' >> ~/.bashrc
-echo 'alias kgrs=\'kubectl get rs \'' >> ~/.bashrc
-echo 'alias kdrs=\'kubectl describe rs \'' >> ~/.bashrc
-echo 'alias kgd=\'kubectl get deploy \'' >> ~/.bashrc
+echo "alias k='kubectl'" >> ~/.bashrc
+echo "alias kgp='kubectl get pods'" >> ~/.bashrc
+echo "alias kgs='kubectl get svc'" >> ~/.bashrc
+echo "alias kdp='kubectl describe pod'" >> ~/.bashrc
+echo "alias kl='kubectl logs -f'" >> ~/.bashrc
+echo "alias klf='kubectl logs -f'" >> ~/.bashrc
+echo "alias kxb='kubectl exec -it'" >> ~/.bashrc
+echo "alias kdesc='kubectl describe'" >> ~/.bashrc
+echo "alias kgettmm='kubectl get pods -owide  | grep tmm | sort -k8'" >> ~/.bashrc
+echo "alias kgrs='kubectl get rs '" >> ~/.bashrc
+echo "alias kdrs='kubectl describe rs '" >> ~/.bashrc
+echo "alias kgd='kubectl get deploy '" >> ~/.bashrc
 
 echo 'source ~/.bash_kube_completion'  >> ~/.bashrc

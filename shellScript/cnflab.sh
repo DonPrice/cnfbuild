edcfa3a48c0cde428b11c3310f7e49bd9af84814
Donovan Price <donovan.price@pricelab.net.au>
2024-05-03 11:23:49 +1000

    fix: added more tmm memory.

--- shellScript/cnflab.sh
+++ shellScript/cnflab.sh
@@ -18,7 +18,7 @@ EOF
 
 USERNAME=ubuntu
 HOSTS="10.1.1.6 10.1.1.7 10.1.1.8 10.1.1.12"
-SCRIPT="sudo chmod 700 /home/ubuntu/55-cnf.yaml; sudo cp /home/ubuntu/55-cnf.yaml /etc/netplan/ sudo netplan apply"
+SCRIPT="sudo chmod 700 /home/ubuntu/55-cnf.yaml; sudo cp /home/ubuntu/55-cnf.yaml /etc/netplan/; sudo netplan apply"
 for HOSTNAME in ${HOSTS} ; do
     scp 55-cnf.yaml ${USERNAME}@${HOSTNAME}:/home/ubuntu/
     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
